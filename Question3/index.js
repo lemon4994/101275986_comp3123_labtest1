1d32a3a5a6f21756dde797419ce9042ca2f6ef29
Nicholas Chiarello <madeupmon@gmail.com>
2024-10-03 18:09:46 -0400

    Question 3

--- Question3/index.js
+++ Question3/index.js
@@ -1,7 +1,7 @@
 const create = require('./create_logs')
 const clear = require('./clear_logs')
 
-const selection = 1 //for testing, change to 0 to create logs and 1 to clear logs
+const selection = 0 //for testing, change to 0 to create logs and 1 to clear logs
 
 if(selection === 0) {
     create()
